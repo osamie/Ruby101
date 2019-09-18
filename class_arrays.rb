ef8f3a4e6e6e271f04dca9eb96f51bd57d340c96
Zuwa Omigie <zuwa.omigie@shopify.com>
2019-09-18 11:30:00 -0400

    More learnings - Package Arrays class into a module - Define a static method using class << self instead of self. - Invoking a class in a module

--- class_arrays.rb
+++ class_arrays.rb
@@ -1,48 +1,52 @@
-class Arrays
+module Ruby101
+    class Arrays
 
-    # Illustrative - Instance variable holding a reference to the array
-    attr_accessor :values 
+        # Illustrative - Instance variable holding a reference to the array
+        attr_accessor :values 
 
-    # Illustrative - Constructor for class
-    def initialize(input = [])
-        @values = input
-        puts "Initializing Instance with #{@values}" 
-    end
+        # Illustrative - Constructor for class
+        def initialize(input = [])
+            @values = input
+            puts "Initializing Instance with #{@values}" 
+        end
 
-    # Given an Array, returns the elements 
-    # that are present exactly once in the array.
-    # This is a static method
-    def self.non_duplicated_values(input_array) 
-        is_visited = {}
-        non_duplicate = []
+        # Given an Array, returns the elements 
+        # that are present exactly once in the array.
+        # This is a static method
 
+        class << self
+            def non_duplicated_values(input_array) 
+                is_visited = {}
+                non_duplicate = []
         
-        (0..input_array.length-1).each { |index| 
-            element = input_array[index]
-
-            if (is_visited[element])
-                is_visited[element] += 1
-            else
-                is_visited[element] = 1
+                
+                (0..input_array.length-1).each { |index| 
+                    element = input_array[index]
+        
+                    if (is_visited[element])
+                        is_visited[element] += 1
+                    else
+                        is_visited[element] = 1
+                    end
+        
+                }
+        
+                # if any entry in isVisited is less than 2, then add to non duplicate
+                is_visited.each {|key,count|
+                    if (count == 1)
+                        non_duplicate.push(key)
+                    end
+                }
+        
+                return "#{non_duplicate}"
             end
-
-        }
-
-        # if any entry in isVisited is less than 2, then add to non duplicate
-        is_visited.each {|key,count|
-            if (count == 1)
-                non_duplicate.push(key)
-            end
-        }
-
-        return "#{non_duplicate}"
+        end
     end
-
 end
 
-r = Arrays.new([1,6,7,3])
+r = Ruby101::Arrays.new([1,6,7,3])
 r.values = [1,2,3]
 puts "Modified instance variable: #{r.values}"
-# puts Arrays.non_duplicated_values([1,2,3,4,5,"you",2,2,9,3])
+puts "Static method invocation: #{Ruby101::Arrays.non_duplicated_values([1,2,3,4,5,"you",2,2,9,3])}"
 
 
